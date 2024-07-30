9bc5334273e43eb1763099e0cc0ae406fc1607d9
Traktuner <78533119+traktuner@users.noreply.github.com>
2024-07-30 14:50:17 +0200

    fix mv path

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -4,7 +4,7 @@ export HOME=/config
 if grep -qi "ubuntu" /etc/os-release; then
     /bin/sh -c MediathekView
 elif grep -qi "alpine" /etc/os-release; then
-    /tmp/MediathekView
+    /opt/MediathekView/MediathekView
 fi
 
 sleep infinity
