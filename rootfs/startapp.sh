e3b8dcc713a7881a02c94e9dea594517139d2a1f
Traktuner <78533119+traktuner@users.noreply.github.com>
2024-10-28 06:32:04 +0100

    add ubuntu24 baseimage

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 export HOME=/config
 
-if grep -qi "ubuntu" /etc/os-release; then
-    /bin/sh -c MediathekView
-elif grep -qi "alpine" /etc/os-release; then
-    /opt/MediathekView/MediathekView
-fi
+/bin/sh -c MediathekView
 
 sleep infinity
